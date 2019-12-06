f153dec69ff62dd58197fff409c7373830ad1120
Ozal Khan <ozalkhan196@gmail.com>
2019-12-05 21:24:20 -0500

    added todo form and used setValue and setTodo to update state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const state = [
+  { text: "learn hooks", isComp: false },
+  { text: "build with K8", isComp: false },
+  { text: "walk doggy", isComp: false }
+];
+
+const Todo = ({ todo, index }) => {
+  return <div>{todo.text}</div>;
+};
+
+const TodoForm = ({ addTodo }) => {
+  // value starts at empty string
+  const [value, setValue] = useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue("");
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={value}
+        onChange={e => setValue(e.target.value)}
+      />
+    </form>
+  );
+};
+
+const App = () => {
+  const [todos, setTodos] = useState(state);
+
+  const addTodo = text => {
+    const newTodos = [...todos, { text }];
+    console.log(setTodos);
+    setTodos(newTodos); //setTodo changes state to passed in arg?
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {todos.map((todo, idx) => (
+          <Todo key={idx} index={idx} todo={todo} />
+        ))}
+      </ul>
+      <TodoForm addTodo={addTodo} />
     </div>
   );
-}
+};
 
 export default App;
